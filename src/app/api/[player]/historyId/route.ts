a9d41dd24ecc198d3e10c4ed98ebc1ca999f9099
Vincent <vynnievert@gmail.com>
2025-05-04 21:33:50 -0400

    started work on historyId

--- src/app/api/[player]/historyId/route.ts
+++ src/app/api/[player]/historyId/route.ts
@@ -6,6 +6,47 @@ const prisma = new PrismaClient();
 
 // This is a placeholder, but its final functionality will be to get the IDs of the last 20 matches played by the queried player
 export default function GET(request: Request){
+    const responseHeaders = {
+        'Access-Control-Allow-Origin': '*',
+    };
+    try{
+        const riotId = params.player;
+        const [gameName, tagLine] = riotId.split('#');
+        const player = await prisma.player.findUnique({
+            where: {
+                gameName: gameName,
+                tagLine: tagLine,
+            },
+        });
+        if (!player) {
+            return NextResponse.json({ error: 'Player not found' }, {
+                status: 404,
+                headers: responseHeaders,
+            });
+        }
+
+        const findLast20Matches = await prisma.playerMatchHistory.findMany({
+            where: {
+                playerId: player.id,
+            },
+            orderBy: {
+                gameStartTime: 'desc',
+            },
+            take: 20,
+        });
+        if (!findLast20Matches) {
+            
+        }
+
+    } catch (error) {
+        console.error('Error fetching player data:', error);
+        return NextResponse.json({ error: 'Internal server error' }, {
+            status: 500,
+            headers: responseHeaders,
+        });
+    }
+    // If everything goes well, return a success response
+    // Note: This is a placeholder response.
     return NextResponse.json({
         message: "Hello from the server!",
         status: 200,
