6d302e5625f47d275419897de5ad3da39b50709c
Vincent <vynnievert@gmail.com>
2025-07-17 22:34:33 -0400

    added some consts,

---
+++ src/lib/regions.ts
@@ -0,0 +1,10 @@
+export const validRegions: string[] = [
+  'br1','eun1','euw1','jp1','kr','la1','la2',
+  'me1','na1','oc1','ru','sg2','tr1','tw2','vn2',
+]
+
+// export type Region = (typeof validRegions)[number];
+
+// export function isValidRegion(region: string): region is Region {
+//     return validRegions.includes(region as Region);
+// }
