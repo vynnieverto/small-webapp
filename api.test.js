e294be61741f46e0deeff52890ad3b47cbd9a5e9
Vincent <vynnievert@gmail.com>
2026-06-20 23:02:58 -0400

    updated gitignore again to ignore local db

--- api.test.js
+++ api.test.js
@@ -5,207 +5,228 @@ const request = require('supertest');
 const BASE_URL = 'http://localhost:3000'; 
 
 
-// describe('getPlayer unit tests', () => {
-//     const PUUID1 = null; // Initialize PUUID variable
-//     const tagLine1 = 'GLHF'; // 
-//     const gameName1 = 'CorruptCosmonaut'; // 
-
-
-//     beforeAll(async () => {
-//         // intialize the database with a player
-//         await request(BASE_URL).post('/api/getPlayer').send({ gameName: 'RocketEscape', tagLine: 'GLHF', region: 'americas' })
-
-
-//     });
-
-
-
-    
-//     describe('POST /api/getPlayer with already queried player', () =>{
-//         it('should return existing player data', async () => {
-//             const response = await request(BASE_URL)
-//                 .post('/api/getPlayer')
-//                 .send({ gameName: 'RocketEscape', tagLine: 'GLHF', region: 'americas' }); // Replace with actual PUUID for testing
-            
-//             expect(response.status).toBe(200);
-//             expect(response.body).toHaveProperty('playerData');
-//             expect(response.body.playerData).toHaveProperty('gameName', 'RocketEscape');
-//             expect(response.body.playerData).toHaveProperty('tagLine', 'GLHF');
-//             expect(response.body.playerData).toHaveProperty('puuid', 'JVoi3ouOBbEKAq8br-HQ5twNfppvUydFQQPZMmfM0hZPUSeplHRpgoqUhxg8lfIvlh3QzoVKFzILig');
-
-//         });
-//     });
-
-
-//     describe('POST /api/getPlayer with new player', () => {
-//         it('should create a new player and return the data', async () => {
-//             const response = await request(BASE_URL)
-//                 .post('/api/getPlayer')
-//                 .send({ gameName: gameName1, tagLine: tagLine1, region: 'americas' });
-            
-//             expect(response.status).toBe(200);
-//             expect(response.body).toHaveProperty('playerData');
-//             expect(response.body.playerData).toHaveProperty('gameName', gameName1);
-//             expect(response.body.playerData).toHaveProperty('tagLine', tagLine1);
-//             expect(response.body.playerData).toHaveProperty('puuid', 'zHmeb_JI_l_TtkISS6y92WBWscwyS4ozF_FR8oR6rqN2iBJtN4FmeoBnsfdxOjSzvJ9AcNRiXZNWsw'); // Check if PUUID is present
-//             expect(response.body.playerData).toHaveProperty('Region', 'na1'); // Check if region is present
-//         });
-//     });
-
-//     describe('POST /api/getPlayer with no data', () => {
-//         it('should return an error for empty player data', async () => {
-//             const response = await request(BASE_URL)
-//                 .post('/api/getPlayer')
-//                 .send({ gameName: '', tagLine: '', region: 'americas' });
-            
-//             expect(response.status).toBe(400); // Assuming 400 for bad request
-//             expect(response.body).toHaveProperty('error');
-//         });
-//     });
-
-//     describe('POST /api/getPlayer with missing data', () => {
-//         it('should return an error for invalid player data', async () => {
-//             const response = await request(BASE_URL)
-//                 .post('/api/getPlayer')
-//                 .send({ gameName: '2', tagLine: '1' });
-            
-//             expect(response.status).toBe(400); // Assuming 400 for bad request
-//             expect(response.body).toHaveProperty('error');
-//         });
-//     });
-
-//     describe('POST /api/getPlayer with invalid region', () => {
-//         it('should return an error for invalid region', async () => {
-//             const response = await request(BASE_URL)
-//                 .post('/api/getPlayer')
-//                 .send({ gameName: gameName1, tagLine: tagLine1, region: 'invalidRegion' });
-            
-//             expect(response.status).toBe(400); // Assuming 400 for bad request
-//             expect(response.body).toHaveProperty('error');
-//         });
-//     });
-
-//     describe('POST /api/getPlayer with no game name', () => {
-//         it('should return an error for invalid game name', async () => {
-//             const response = await request(BASE_URL)
-//                 .post('/api/getPlayer')
-//                 .send({ gameName: '', tagLine: tagLine1, region: 'americas' });
-            
-//             expect(response.status).toBe(400); // Assuming 400 for bad request
-//             expect(response.body).toHaveProperty('error');
-//         });
-//     });
-
-//     describe('POST /api/getPlayer with no tag line', () => {
-//         it('should return an error for invalid tag line', async () => {
-//             const response = await request(BASE_URL)
-//                 .post('/api/getPlayer')
-//                 .send({ gameName: gameName1, tagLine: '', region: 'americas' });
-            
-//             expect(response.status).toBe(400); // Assuming 400 for bad request
-//             expect(response.body).toHaveProperty('error');
-//         });
-//     });
-
-//     describe('POST /api/getPlayer with no data', () => {
-//         it('should return an error for invalid region', async () => {
-//             const response = await request(BASE_URL)
-//                 .post('/api/getPlayer')
-//                 .send({});
-            
-//             expect(response.status).toBe(400); // Assuming 400 for bad request
-//             expect(response.body).toHaveProperty('error');
-//         });
-//     });
-
-
-// });
-
-
-describe('[player]/Mastery unit tests', () => {
+describe('getPlayer integration tests', () => {
     const PUUID1 = null; // Initialize PUUID variable
     const tagLine1 = 'GLHF'; // 
     const gameName1 = 'CorruptCosmonaut'; // 
 
+
     beforeAll(async () => {
         // intialize the database with a player
-        await request(BASE_URL).post('/api/getPlayer').send({ gameName: 'RocketEscape', tagLine: 'GLHF', region: 'americas' })
-        await request(BASE_URL).get('/api/RocketEscape-GLHF/Mastery').send({ gameName: 'RocketEscape', tagLine: 'GLHF', region: 'americas' })
-        await request(BASE_URL).post('/api/getPlayer').send({ gameName: gameName1, tagLine: tagLine1, region: 'americas' })
-
-
+        await request(BASE_URL).post('/api/getPlayer').send({ gameName: 'RocketEscape', tagLine: 'GLHF', platform: 'na1' })
     });
 
 
-    describe('GET /api/player/mastery with already queried player', () =>{
-        it('should return existing player data', async () => {
-            const response = await request(BASE_URL)
-                .get('/api/RocketEscape-GLHF/Mastery')
-            
-            expect(response.status).toBe(200);
-            expect(Array.isArray(response.body)).toBe(true); // Check if playerData is an array
-            expect(response.body.length).toBeGreaterThan(0); // Check if playerData is not empty
-            expect(response.body[0]).toHaveProperty('championId'); // Check if championId is present
-            expect(response.body[0]).toHaveProperty('championLevel'); // Check if championLevel is present
-            expect(response.body[0]).toHaveProperty('championPoints'); // Check if championPoints is present
-
-            
-
-        });
-    });
-
-    describe('GET /api/player/mastery with no saved mastery data', () => {
-        it('should query the riot api and return the data', async () => {
-            const response = await request(BASE_URL)
-                .get('/api/RocketEscape-GLHF/Mastery')
-            
-                expect(response.status).toBe(200);
-                expect(Array.isArray(response.body)).toBe(true); // Check if playerData is an array
-                expect(response.body.length).toBeGreaterThan(0); // Check if playerData is not empty
-                expect(response.body[0]).toHaveProperty('championId'); // Check if championId is present
-                expect(response.body[0]).toHaveProperty('championLevel'); // Check if championLevel is present
-                expect(response.body[0]).toHaveProperty('championPoints'); // Check if championPoints is present
-        });
-    });
-
-    describe('GET /api/player/mastery with a player not in the db saved player', () => {
-        it('should return an error for player data', async () => {
-            const response = await request(BASE_URL)
-                .get('/api/AloisNL-EUW/Mastery')
-            
-            expect(response.status).toBe(400); // Assuming 400 for bad request
-            expect(response.body).toHaveProperty('error');
-        });
-    });
-
-
-    describe('GET /api/player/mastery with no region', () => {
-        it('should return an error for invalid region', async () => {
-            const response = await request(BASE_URL)
-                .get('/api/RocketEscape-GLHF/Mastery')
-            
-            expect(response.status).toBe(400); // Assuming 400 for bad request
-            expect(response.body).toHaveProperty('error');
-        });
-    });
 
     
+    describe('POST /api/getPlayer with already queried player', () =>{
+        it('should return existing player data', async () => {
+            const response = await request(BASE_URL)
+                .post('/api/getPlayer')
+                .send({ gameName: 'RocketEscape', tagLine: 'GLHF', platform: 'na1' }); // Replace with actual PUUID for testing
+            
+            expect(response.status).toBe(200);
+            expect(response.body).toHaveProperty('playerData');
+            expect(response.body.playerData).toHaveProperty('gameName', 'RocketEscape');
+            expect(response.body.playerData).toHaveProperty('tagLine', 'GLHF');
+            expect(response.body.playerData).toHaveProperty('puuid', 'JVoi3ouOBbEKAq8br-HQ5twNfppvUydFQQPZMmfM0hZPUSeplHRpgoqUhxg8lfIvlh3QzoVKFzILig');
+
+        });
+    });
+
+
+    describe('POST /api/getPlayer with new player', () => {
+        it('should create a new player and return the data', async () => {
+            const response = await request(BASE_URL)
+                .post('/api/getPlayer')
+                .send({ gameName: gameName1, tagLine: tagLine1, platform: 'na1' });
+            
+            if (response.status !== 200) {
+            console.error(response.body);
+            }
+            expect(response.status).toBe(200);
+            expect(response.body).toHaveProperty('playerData');
+            expect(response.body.playerData).toHaveProperty('gameName', gameName1);
+            expect(response.body.playerData).toHaveProperty('tagLine', tagLine1);
+            expect(response.body.playerData).toHaveProperty('puuid', 'zHmeb_JI_l_TtkISS6y92WBWscwyS4ozF_FR8oR6rqN2iBJtN4FmeoBnsfdxOjSzvJ9AcNRiXZNWsw'); // Check if PUUID is present
+            expect(response.body.playerData).toHaveProperty('Region', 'na1'); // Check if platform is present
+        });
+    });
+
+    describe('POST /api/getPlayer with no data', () => {
+        it('should return an error for empty player data', async () => {
+            const response = await request(BASE_URL)
+                .post('/api/getPlayer')
+                .send({ gameName: '', tagLine: '', platform: 'na1' });
+            
+            expect(response.status).toBe(400); // Assuming 400 for bad request
+            expect(response.body).toHaveProperty('error');
+        });
+    });
+
+    describe('POST /api/getPlayer with missing data', () => {
+        it('should return an error for invalid player data', async () => {
+            const response = await request(BASE_URL)
+                .post('/api/getPlayer')
+                .send({ gameName: '2', tagLine: '1' });
+
+            if (response.status !== 400) {
+            console.error(response.body);
+            }
+
+            expect(response.status).toBe(400); // Assuming 400 for bad request
+            expect(response.body).toHaveProperty('error');
+        });
+    });
+
+    describe('POST /api/getPlayer with invalid platform', () => {
+        it('should return an error for invalid platform', async () => {
+            const response = await request(BASE_URL)
+                .post('/api/getPlayer')
+                .send({ gameName: gameName1, tagLine: tagLine1, platform: 'invalidplatform' });
+
+            if (response.status !== 400) {
+            console.error(response.body);
+            }
+
+            expect(response.status).toBe(400); // Assuming 400 for bad request
+            expect(response.body).toHaveProperty('error');
+        });
+    });
+
+    describe('POST /api/getPlayer with no game name', () => {
+        it('should return an error for invalid game name', async () => {
+            const response = await request(BASE_URL)
+                .post('/api/getPlayer')
+                .send({ gameName: '', tagLine: tagLine1, platform: 'na1' });
+
+            if (response.status !== 400) {
+            console.error(response.body);
+            }
+
+            expect(response.status).toBe(400); // Assuming 400 for bad request
+            expect(response.body).toHaveProperty('error');
+        });
+    });
+
+    describe('POST /api/getPlayer with no tag line', () => {
+        it('should return an error for invalid tag line', async () => {
+            const response = await request(BASE_URL)
+                .post('/api/getPlayer')
+                .send({ gameName: gameName1, tagLine: '', platform: 'na1' });
+            
+            if (response.status !== 400) {
+            console.error(response.body);
+            }
+
+            expect(response.status).toBe(400); // Assuming 400 for bad request
+            expect(response.body).toHaveProperty('error');
+        });
+    });
+
+    describe('POST /api/getPlayer with no data', () => {
+        it('should return an error for invalid platform', async () => {
+            const response = await request(BASE_URL)
+                .post('/api/getPlayer')
+                .send({});
+            
+            if (response.status !== 400) {
+            console.error(response.body);
+            }
+
+            expect(response.status).toBe(400); // Assuming 400 for bad request
+            expect(response.body).toHaveProperty('error');
+        });
+    });
+
 
 });
 
 
-
-
-// describe('[player]/MatchHistory unit tests', () => {
+// describe('[player]/Mastery integration tests', () => {
 //     const PUUID1 = null; // Initialize PUUID variable
 //     const tagLine1 = 'GLHF'; // 
 //     const gameName1 = 'CorruptCosmonaut'; // 
 
 //     beforeAll(async () => {
 //         // intialize the database with a player
-//         await request(BASE_URL).post('api/getPlayer').send({ gameName: 'RocketEscape', tagLine: 'GLHF', region: 'americas' })
-//         await request(BASE_URL).get('api/RocketEscape#GLHF/MatchHistory').send({ gameName: 'RocketEscape', tagLine: 'GLHF', region: 'americas' })
-//         await request(BASE_URL).post('api/getPlayer').send({ gameName: gameName1, tagLine: tagLine1, region: 'americas' })
+//         await request(BASE_URL).post('/api/getPlayer').send({ gameName: 'RocketEscape', tagLine: 'GLHF', platform: 'na1' })
+//         await request(BASE_URL).get('/api/RocketEscape-GLHF/Mastery').send({ gameName: 'RocketEscape', tagLine: 'GLHF', platform: 'na1' })
+//         await request(BASE_URL).post('/api/getPlayer').send({ gameName: gameName1, tagLine: tagLine1, platform: 'na1' })
+
+
+//     });
+
+
+//     describe('GET /api/player/mastery with already queried player', () =>{
+//         it('should return existing player data', async () => {
+//             const response = await request(BASE_URL)
+//                 .get('/api/RocketEscape-GLHF/Mastery')
+            
+//             expect(response.status).toBe(200);
+//             expect(Array.isArray(response.body)).toBe(true); // Check if playerData is an array
+//             expect(response.body.length).toBeGreaterThan(0); // Check if playerData is not empty
+//             expect(response.body[0]).toHaveProperty('championId'); // Check if championId is present
+//             expect(response.body[0]).toHaveProperty('championLevel'); // Check if championLevel is present
+//             expect(response.body[0]).toHaveProperty('championPoints'); // Check if championPoints is present
+
+            
+
+//         });
+//     });
+
+//     describe('GET /api/player/mastery with no saved mastery data', () => {
+//         it('should query the riot api and return the data', async () => {
+//             const response = await request(BASE_URL)
+//                 .get('/api/RocketEscape-GLHF/Mastery')
+            
+//                 expect(response.status).toBe(200);
+//                 expect(Array.isArray(response.body)).toBe(true); // Check if playerData is an array
+//                 expect(response.body.length).toBeGreaterThan(0); // Check if playerData is not empty
+//                 expect(response.body[0]).toHaveProperty('championId'); // Check if championId is present
+//                 expect(response.body[0]).toHaveProperty('championLevel'); // Check if championLevel is present
+//                 expect(response.body[0]).toHaveProperty('championPoints'); // Check if championPoints is present
+//         });
+//     });
+
+//     describe('GET /api/player/mastery with a player not in the db saved player', () => {
+//         it('should return an error for player data', async () => {
+//             const response = await request(BASE_URL)
+//                 .get('/api/AloisNL-EUW/Mastery')
+            
+//             expect(response.status).toBe(400); // Assuming 400 for bad request
+//             expect(response.body).toHaveProperty('error');
+//         });
+//     });
+
+
+//     describe('GET /api/player/mastery with no platform', () => {
+//         it('should return an error for invalid platform', async () => {
+//             const response = await request(BASE_URL)
+//                 .get('/api/RocketEscape-GLHF/Mastery')
+            
+//             expect(response.status).toBe(400); // Assuming 400 for bad request
+//             expect(response.body).toHaveProperty('error');
+//         });
+//     });
+
+    
+
+// });
+
+
+
+
+// describe('[player]/MatchHistory integration tests', () => {
+//     const PUUID1 = null; // Initialize PUUID variable
+//     const tagLine1 = 'GLHF'; // 
+//     const gameName1 = 'CorruptCosmonaut'; // 
+
+//     beforeAll(async () => {
+//         // intialize the database with a player
+//         await request(BASE_URL).post('api/getPlayer').send({ gameName: 'RocketEscape', tagLine: 'GLHF', platform: 'na1' })
+//         await request(BASE_URL).get('api/RocketEscape#GLHF/MatchHistory').send({ gameName: 'RocketEscape', tagLine: 'GLHF', platform: 'na1' })
+//         await request(BASE_URL).post('api/getPlayer').send({ gameName: gameName1, tagLine: tagLine1, platform: 'na1' })
 
 
 //     });
