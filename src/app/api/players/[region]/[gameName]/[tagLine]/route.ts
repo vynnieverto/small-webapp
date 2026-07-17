28f4b9eb2445ef8a12bce7bfdc64a3c6203fac98
Vincent <vynnievert@gmail.com>
2026-07-17 19:57:32 -0400

    satisfy ts

--- src/app/api/players/[region]/[gameName]/[tagLine]/route.ts
+++ src/app/api/players/[region]/[gameName]/[tagLine]/route.ts
@@ -2,8 +2,9 @@ import { NextRequest, NextResponse } from 'next/server';
 import {getPlayerProfile} from "@/services/get_player_profile";
 
 
-export async function GET(request: NextRequest, params: {gameName: string, tagLine: string, platform: string}){
-    const result = await getPlayerProfile(params)
+export async function GET(request: NextRequest, { params }: { params: Promise<{gameName: string, tagLine: string, platform: string }> }){
+    const values =await params
+    const result = await getPlayerProfile(values)
     return NextResponse.json(result.body, 
         {status: result.status}
     )
